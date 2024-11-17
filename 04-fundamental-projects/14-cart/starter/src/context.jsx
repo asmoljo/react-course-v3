17e10c7b40cd24a87d2b7b1e5bc615c69427182d
asmoljo <smoljo.antonio@gmail.com>
2024-11-17 22:28:12 +0100

    a

--- 04-fundamental-projects/14-cart/starter/src/context.jsx
+++ 04-fundamental-projects/14-cart/starter/src/context.jsx
@@ -1,14 +1,23 @@
 import { useContext, useReducer, useEffect, createContext } from 'react';
 import reducer from './reducer';
-import { CLEAR_CART, REMOVE, INCREASE, DECREASE } from './actions';
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from './actions';
 import cartItems from './data';
 import { getTotals } from './utils';
+
+const url = 'https://www.course-api.com/react-useReducer-cart-project';
 
 const AppContext = createContext();
 
 const initialState = {
   loading: false,
-  cart: new Map(cartItems.map((item) => [item.id, item])),
+  cart: new Map(),
 };
 
 export const AppProvider = ({ children }) => {
@@ -31,6 +40,16 @@ export const AppProvider = ({ children }) => {
   const decrease = (id) => {
     dispatch({ type: DECREASE, payload: { id } });
   };
+  const fetchData = async () => {
+    dispatch({ type: LOADING });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: DISPLAY_ITEMS, payload: { cart } });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <AppContext.Provider
